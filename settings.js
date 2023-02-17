dfff714a7984ebc6c82ad50e3a06d0866bdd1fd4
hwahwhahahwh <125477689+hwahwhahahwh@users.noreply.github.com>
2023-02-17 12:09:40 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,12 +4,12 @@ const  { indonesia, english, spanyol} = require(`./language`)
 
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
-	alfa: 'https://youtube.com/@emperoranus7621', //apabila link api eror, segera laporkan ke owner
+	alfa: 'htps://youtube.com/@emperoranus7621', //apabila link api eror, segera laporkan ke owner
 }
 
 // Free apikey (Apikey expired silahkan login terus ganti V12 dgn apikey lu)
 global.APIKeys = {
-	'https://youtube.com/@emperoranus7621': 'V12',  // 👉 login https://api.zeeoneofc.xyz to get apikey
+	'htps://youtube.com/@emperoranus7621': 'V12',  // 👉 login https://api.zeeoneofc.xyz to get apikey
 }
 
 // setting 
@@ -29,22 +29,22 @@ global.language = english  //change indonesia to english if you don't understand
 // Other
 global.botname = "EMPERORANUS" //namabot kalian
 global.ownername= "ᴹᴿ᭄ EMPERORANUS ×፝֟͜×" //nama kalian
-global.myweb ="https://youtube.com/@emperoranus7621" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@emperoranus7621" //bebas asal jan hapus
+global.myweb ="htps://youtube.com/@emperoranus7621" //bebas asal jan hapus
+global.youtube = "htps://youtube.com/@emperoranus7621" //bebas asal jan hapus
 global.myweb2 = "https://youtube.com/@emperoranus7621" //bebas
-global.email = "967771160204@gmail.com" //bebas
+global.email = "97474727015@gmail.com" //bebas
 global.region = "Sri Lanka" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['967771160204'] //premium user
-global.owner = ["967771160204"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "967771160204" // nomor wa kalian
-global.ownernomerr = "967771160204" //nmr wa kalian
+global.premium = ['97474727015'] //premium user
+global.owner = ["97474727015"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "97474727015" // nomor wa kalian
+global.ownernomerr = "97474727015" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/1592262447c5ce8714b60.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/1592262447c5ce8714b60.jpg'
-global.packname = '© حقوق عمك انوس-Mdོ' //sticker wm ubah
-global.author = 'حقوق انوس' //sticker wm ganti nama kalian
+global.packname = 'حقوق عمك ريوزاكي ☕-Mdོ' //sticker wm ubah
+global.author = 'حقوق  ريوزاكي☕ ' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
